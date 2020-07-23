551ee8d039c865ba3b26114b32b3f9de1621a990
Walter Melo <walmello24@gmail.com>
2020-07-23 15:55:27 -0300

    testemunhos

--- components/Testimonial.vue
+++ components/Testimonial.vue
@@ -10,6 +10,19 @@ h3, h4{
 	font-weight: normal;
 }
 
+.row {
+	flex-wrap: wrap;
+	align-items: flex-start;
+	justify-content: center;
+}
+
+.item {
+	max-width: 15rem;
+}
+
+.item {
+
+}
 
 @media (min-width: 700px){
 	section {
@@ -27,17 +40,64 @@ h3, h4{
 
 </style>
 
+<script>
+	let items = []
+
+	items.push({
+		img: 'https://media-exp1.licdn.com/dms/image/C4D03AQF9npZ0RcFWPw/profile-displayphoto-shrink_200_200/0?e=1600905600&v=beta&t=cTXYrCJbs1fDaC-JwL4oIPtVT2tF0XFmkE_8hMaGmjM',
+		nome: 'Anderson Kleyton',
+		titulo: 'Inovação Transformação Digital Indústria 4.0 - RevTec',
+		content: `… oferece serviços aderentes a necessidade de cada cliente. Em um mercado onde a maioria tenta “empurrar goela baixo” seus produtos, este é um diferencial.`
+
+	})
+
+	items.push({
+		img: 'https://media-exp1.licdn.com/dms/image/C4D03AQH_9cKznSa8UA/profile-displayphoto-shrink_200_200/0?e=1600905600&v=beta&t=M_0QkbyNkZo6B0Ne41n9hnj-GcRqtVtPQDbNc4uqkZA',
+		nome: 'Geisa Luz',
+		titulo: 'Consultora em Doenças Raras | Palestrante | Pós doutora em Saúde Pública | Enfermeira',
+		content: `… Já contratei um dos seus serviços … e os resultados foram surpreendentes. Sou muito grata!`
+
+	})
+
+	items.push({
+		img: 'https://media-exp1.licdn.com/dms/image/C4D03AQEpP8xjQpch_A/profile-displayphoto-shrink_200_200/0?e=1600905600&v=beta&t=FmwCLEz8jrjx-arSGIWxHgHb8YQlHOuCv5uuIvm1RiA',
+		nome: 'Matheus Celestino',
+		titulo: 'Executivo de Negócios, Resultados Digitais',
+		content: `… No inicio de 2019, a Hacking Growth Inovação fechou uma parceria estratégica com a Resultados Digitais, e tive o enorme prazer de fazer essa ponte... Uma empresa totalmente focada em entrega e resultado de seus respectivos clientes. Sempre muito focada naquilo que se propõe a entregar.`
+
+	})
+	items.push({
+		img: 'https://media-exp1.licdn.com/dms/image/C4D03AQFWHshtL6DLMA/profile-displayphoto-shrink_200_200/0?e=1600905600&v=beta&t=TZ66upwy0cLZvBidZdBlGJarJkFmY2jVZ3xAYp18xbY',
+		nome: 'Renata Costa',
+		titulo: 'Gestora e Consultora de Educação a Distância (EaD) l Professora de Tecnologia da Informação l Conteudista de TI',
+		content: `… Excelente sua atuação como Growth Hacker. Nossos diálogos e trocas de informações fizeram o meu SSI decolar!`
+
+	})
+
+	export default {
+		data(){
+			return {
+				items
+			}
+		}
+	}
+</script>
+
 <template>
 	<section id="depoimentos">
 		<h2>Veja algumas opiniões de quem já fechou negócio conosco</h2>
 		<div class="row">
-			<img src="">
-			<div>
-				<h3><em>Anderson Kleyton</em></h3>
-				<h4>Inovação Transformação Digital Indústria 4.0 - RevTec</h4>
-				<p>
-					“… oferece serviços aderentes a necessidade de cada cliente. Em um mercado onde a maioria tenta “empurrar goela baixo” seus produtos, este é um diferencial.”
-				</p>				
+			<div v-for="item in items" class="item">
+				<div>
+					<img v-bind:src="item.img">
+				</div>								
+				<div>
+					<h3><em>{{ item.nome }}</em></h3>
+					<h4>{{ item.titulo }}</h4>
+					<p>
+						“{{ item.content }}”
+					</p>
+				</div>
 			</div>
 		</div>
 	</section>
